93537c7a00185a4d063c244353f76feeb4d46971
Syeoryn <drewcuth1@gmail.com>
2014-03-22 17:32:51 -0700

    Stringifies strings, numbers, booleans, and functions.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,24 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+  var type =typeof obj;
+	if( type == 'object' )	type = Array.isArray(obj) ? 'array' : 'object';
+	switch(true){
+		case type == 'string':
+			return "" + "\"" + obj + "\"";
+			break;
+		case type == 'number':
+			return "" + obj;
+			break;
+		case type == 'boolean':
+			return "" + obj;
+			break;
+		case type == 'function':
+			return undefined
+			break;
+		case type == 'array':
+			break;
+		case type = 'object':
+			break;
+	}
 };
