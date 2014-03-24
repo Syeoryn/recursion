2b52ec61a96466facaaa20fc93b53638a845e33e
Syeoryn <drewcuth1@gmail.com>
2014-03-24 14:55:28 -0700

    getElementsByClassName iterates over all nodes

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,16 @@
 
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
-  // your code here
+  var elements = [];
+	if(this.childNodes.length > 0){
+		for(var i = 0; i < this.childNodes.length){
+			var classes = this.childNodes[i].classList;
+			/* Iterate over classes to find if it has className,
+					then elements.push(thing).
+			*/
+			var newElements = getElementsByClassName.apply(this.childNodes[i],[className])
+			for(var j = 0; j < newElements.length; j++) elements.push(newElements[j]);
+		}
+	}
+	return elements
 };
