2a01b2245e8012e79a19ab20da3219994a87e82d
Syeoryn <drewcuth1@gmail.com>
2014-03-25 16:10:41 -0700

    Refactored and added comments

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,22 +4,24 @@
 // };
 
 // But in stead we're going to implement it from scratch:
-var getElementsByClassName = function (className) {
-	var that = arguments[1]? arguments[1] : document;
+var getElementsByClassName = function (className, that) {
+	if(!that) that = document;
+	that = that.childNodes
   var elements = [];
-	if(that.childNodes.length > 0){
-		for(var i = 0; i < that.childNodes.length; i++){
-			var classes = that.childNodes[i].classList;
-			/* Iterate over classes to find if it has className,
-					then elements.push(thing).
-			*/
+	// loop over all child nodes
+	if(that.length > 0){
+	// if that node has className, add it to elements
+		for(var i = 0; i < that.length; i++){
+			var classes = that[i].classList;
 			for(key in classes){
 				if(classes[key] == className) {
-					elements.push(that.childNodes[i]);
+					elements.push(that[i]);
 					break;
 				}
 			}
-			var nestedElements = getElementsByClassName(className,that.childNodes[i]);
+	// now repeat for each node nested in current child node
+			var nestedElements = getElementsByClassName(className,that[i]);
+	// and add non-undefined elements one at a time to elements
 			for(var j = 0; j < nestedElements.length; j++) if(nestedElements[j]) elements.push(nestedElements[j]);
 		}
 	}
