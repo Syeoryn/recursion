bdb76e3be70e9739a7e0e3c3db06b82d5fa74c2c
Syeoryn <drewcuth1@gmail.com>
2014-03-25 15:50:19 -0700

    getElementsByClassName works without .apply.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,20 +5,21 @@
 
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
+	var that = arguments[1]? arguments[1] : document;
   var elements = [];
-	if(this.childNodes.length > 0){
-		for(var i = 0; i < this.childNodes.length; i++){
-			var classes = this.childNodes[i].classList;
+	if(that.childNodes.length > 0){
+		for(var i = 0; i < that.childNodes.length; i++){
+			var classes = that.childNodes[i].classList;
 			/* Iterate over classes to find if it has className,
 					then elements.push(thing).
 			*/
 			for(key in classes){
 				if(classes[key] == className) {
-					elements.push(this.childNodes[i]);
+					elements.push(that.childNodes[i]);
 					break;
 				}
 			}
-			var nestedElements = getElementsByClassName.apply(this.childNodes[i],[className]);
+			var nestedElements = getElementsByClassName(className,that.childNodes[i]);
 			for(var j = 0; j < nestedElements.length; j++) if(nestedElements[j]) elements.push(nestedElements[j]);
 		}
 	}
